85adb98e05ec65273eab17013e5688dcaaf8b87e
Dylan Kay <dylan@tech.melbourne>
2021-09-24 23:01:49 +1000

    remove env vars and use hash router instead

--- src/App.tsx
+++ src/App.tsx
@@ -22,28 +22,9 @@ import { createTheme, ThemeProvider } from "@material-ui/core";
 import PageOne from "./pages/PageOne";
 import PageTwo from "./pages/PageTwo";
 import PageThree from "./pages/PageThree";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-const treasury = new anchor.web3.PublicKey(
-  process.env.REACT_APP_TREASURY_ADDRESS!
-);
-
-const config = new anchor.web3.PublicKey(
-  process.env.REACT_APP_CANDY_MACHINE_CONFIG!
-);
-
-const candyMachineId = new anchor.web3.PublicKey(
-  process.env.REACT_APP_CANDY_MACHINE_ID!
-);
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
-
-const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
-const connection = new anchor.web3.Connection(rpcHost);
-
-const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
-
-const txTimeout = 30000; // milliseconds (confirm this works for your project)
 
 const theme = createTheme({
   palette: {
